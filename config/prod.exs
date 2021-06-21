3b075f2e87bebccbe901796725b421c546fad847
Jhefrey Sajot <heifurukawa1997@gmail.com>
2021-06-21 21:59:36 +0800

    chore: configurations for deployments

--- config/prod.exs
+++ config/prod.exs
@@ -10,11 +10,15 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :tabi, TabiWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [
+    host: Application.get_env(:tabi, :app_hostname),
+    port: Application.get_env(:tabi, :app_port)
+  ]
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :tabi, TabiWeb.Endpoint, server: true
 
 # ## SSL Support
 #
@@ -49,7 +53,3 @@ config :logger, level: :info
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
-
-# Finally import the config/prod.secret.exs which loads secrets
-# and configuration from environment variables.
-import_config "prod.secret.exs"
